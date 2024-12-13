ba1d7e789348b1a2884d6ec52508c8fa7d9ac908
Alexander Z <lulzsecer@gmail.com>
2024-12-13 15:03:59 +0200

    setup envirment (web server)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/timezone-test', function () {
+    return Date::now()->format('Y-m-d H:i:s');
+});
